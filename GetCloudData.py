2bc74e05adeaa9479ba848241f9d2fd8459f0332
Nick Barron <nickb@kentico.com>
2017-08-30 12:37:35 -0500

    New working update

--- GetCloudData.py
+++ GetCloudData.py
@@ -5,42 +5,70 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 counter = 1
 project_id = "dfc80764-b4d6-4554-aa5f-805c3b2dbef1"
 while True:
-    response = requests.get("https://deliver.kenticocloud.com/" + project_id + "/items?system.type=event")
-    #print(response.text)
+    print "Getting Data From Cloud"
+    response = requests.get("https://deliver.kenticocloud.com/" + project_id + "/items?system.type=coffee")
+    print "got the data"
     cloudItems = response.json()
-    print(cloudItems)
-    elements = []
-   
-    print(counter)
-    
+    #print(cloudItems)
+      
+    # elements in the "Coffee" content type
+    # Coffee Codename: 'coffee'
+    # System: 
+    #   name                    i['system']['name']
+    # Elements: 
+    #   product_name            i['elements']['product_name']['value']
+    #   price                   i['elements']['price']['value']
+    #   image[]                 i['elements']['image']['value'][0]['name']
+    #   short_description       
+    #   long_description        
+    #   product_status (tax)
+    #   farm
+    #   country
+    #   variety
+    #   processing
+    #   altitude
+    print "Jumping into a loop of data"
     for i in cloudItems['items']:
+        elements = []
         elements.append(dict(
-            title = i['system']['name'],
-            overlay = i['elements']['short_description']['value'],
-            background = i['elements']['event_image___hi_res']['value'][0]['name'],
-	    contactEmail = i['elements']['contact_email']['value'],
-	    contactPhone = i['elements']['contact_phone_number']['value'],
-	    eventDate = i['elements']['date_of_event']['value'],
-            #backgroundColor = i['elements']['background_color']['value'][0]['codename'],
-            ))
+            product_name = i['elements']['product_name']['value'],
+            price = i['elements']['price']['value'],
+            image = i['elements']['image']['value'][0]['name'],
+            short_description = i['elements']['short_description']['value'],
+            #long_description = i['elements']['long_description']['value'],
+           # product_status (tax)
+            farm = i['elements']['farm']['value'],
+            country = i['elements']['country']['value'],
+            #variety = i['elements']['variety']['value'],
+            #processing = i['elements']['processing']['value'],
+            #altitude = i['elements']['altitude']['value'],
+	    ))
 
-    exisitingJson = json.loads(open('CloudData.json').read())
-    
-    a, b =json.dumps(elements, sort_keys=True),json.dumps(exisitingJson, sort_keys=True)
-    
-    if a != b:
-         #download image
-        image = requests.get(cloudItems['items'][0]['elements']['event_image___hi_res']['value'][0]['url'])
-        with open(cloudItems['items'][0]['elements']['event_image___hi_res']['value'][0]['name'],"wb") as f:
+        exisitingJson = json.loads(open('CloudData.json').read())
+        print "I have the item as JSON"
+        a, b =json.dumps(elements, sort_keys=True),json.dumps(exisitingJson, sort_keys=True)
+        #for i in cloudItems['items']:
+                #if a != b:
+                 #download image
+        print "I am downloading an image"
+        image = requests.get(i['elements']['image']['value'][0]['url'])
+        with open(i['elements']['image']['value'][0]['name'],"wb") as f:
             f.write(image.content)
             f.close()
-        #End Download Image
+                #End Download Image
         with open('CloudData.json','w') as outfile:
             json.dump(elements,outfile)
-            print(elements)
-   # cloudSocket = "KenticoCloud/update:%s"
-   # SendLoad = cloudSocket % data
+        print "The image is downloaded"
+        elements = json.dumps(elements, ensure_ascii=False)
+        print(elements)
+
+        #TODO Send Info Over a Socket to the InfoBeamer
+        coffeeUpdate = "KenticoCloud/update:%s"
+
+        SendLoad = coffeeUpdate % elements
     
-   # sock.sendto(SendLoad.encode("utf8"), ('127.0.0.1', 4444))
-    counter = counter + 1
-    time.sleep(15)
+        sock.sendto(SendLoad.encode("utf8"), ('127.0.0.1', 4444))
+        print "The load is sent!"
+        counter = counter + 1
+        print counter
+        time.sleep(15)
--- node.lua
+++ node.lua
@@ -2,15 +2,92 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 local json = require "json"
 local font = resource.load_font "font.ttf"
+
+node.alias "KenticoCloud"
 local current_image = resource.load_image("image.jpg")
 local cloud_img_name = ""
 local elements = {}
 local counter = 0
 local cache = false
-
-node.alias "KenticoCloud"
-
+local coffeeElements = {}
 local lines = {}
+local image_height = HEIGHT
+local image_width = WIDTH
+local image_align = 0 
+local text_align = WIDTH / 2
+util.data_mapper{
+	
+	update = function(elements)
+		coffeeElements = json.decode(elements)
+		for idx, element in ipairs(coffeeElements) do
+			print(element)
+			print(element.image)
+			local image = string.format("%s", element.image)
+			cloud_img_name = image
+			current_image = resource.load_image(cloud_img_name)
+		end
+		
+		if image_align == 0 then
+			image_align = WIDTH / 2
+			text_align = 0
+		else
+			image_align = 0
+			text_align = WIDTH / 2
+		end
+		image_width = WIDTH / 2 + image_align
+		print(image_align)
+	end
+}
+
+function node.render()
+	gl.clear(0,0,0,1)
+	current_image:draw(image_align,0,image_width,HEIGHT)
+
+	for idx, element in ipairs(coffeeElements) do
+		WriteSlide(element)
+	end
+
+
+end
+
+function WriteSlide(element)
+        local y = 300
+
+ 	font:write(text_align, 10, "New Brews", 100, 1,1,.3,1)
+	--font:write(30, 110, "Demo Time", 100, .5,.5,.5,1)
+        local productName = string.format("%s", element.product_name)
+        font:write(text_align, y, productName, 50, 1,1,.3,1)
+    	y = y + 50
+
+	local price = "$" .. string.format("%s" ,element.price) 
+	font:write(text_align ,y, price, 50,1,1,1,1)
+	y = y +100
+
+        local shortDescription = string.format("%s", element.short_description)
+	shortDescription = shortDescription:gsub("%b<>","")
+	lines = wrap(shortDescription, 35)
+	
+	local size = 50	
+    	for i, line in ipairs(lines) do
+        	font:write(text_align, y, line, size, 1, 1, 1, 1)
+        	y = y + size
+    	end	
+	y = y + size
+	
+	local farm = string.format("%s",element.farm)
+	farm = farm:gsub("%b<>","")
+	font:write(text_align, y, "Farm:", size, 1,1,1,1)
+	y = y + size
+
+	font:write(text_align, y, farm, size, 1,1,1,1)
+	y = y + size + size
+        
+	local country = string.format("%s",element.country)
+	font:write(text_align,y, "Country: " .. country, size,1,1,1,1)
+
+        --socket.sleep(10)
+end
+
 
 function wrap(str, limit, indent, indent1)
     indent = indent or ""
@@ -34,57 +111,3 @@ function wrap(str, limit, indent, indent1)
     end
     return splitted
 end
-
-local CloudData
-util.file_watch("CloudData.json", function(content)
-	elements = json.decode(content)
-	
-	for idx, element in ipairs(elements) do
-		local line4 = string.format("%s", element.background)
-		cloud_img_name = line4
-	end
-	current_image = resource.load_image(cloud_img_name)
-	
-end)
-pp(CloudData)
-
-function node.render()
-    local y = 300
-    for idx, element in ipairs(elements) do
-
-	cloud_img_name = line4
-
-  	current_image:draw(0,0,WIDTH,HEIGHT)
- 	font:write(500, 10, "Dancing Goat Events", 100, 1,1,1,1)
-	--font:write(30, 110, "Demo Time", 100, .5,.5,.5,1)
-        local line = string.format("%s", element.title)
-        font:write(WIDTH / 2, y, line, 50, 1,1,.3,1)
-    	y = y + 50
-	local eventDate = string.format("%s" ,element.eventDate) 
-	--eventDate = os.date("%c", eventDate)
-	font:write(WIDTH / 2 ,y, eventDate, 50,1,1,1,1)
-	y = y +100
-        local line2 = string.format("%s", element.overlay)
-	line2 = line2:gsub("%b<>","")
-	lines = wrap(line2, 35)
-	
-	local size = 50	
-    	for i, line in ipairs(lines) do
-        	font:write(WIDTH / 2, y, line, size, 1, 1, 1, 1)
-        	y = y + size
-    	end	
-	y = y + size
-	
-	local phoneNumber = "Phone Number: " .. string.format("%s",element.contactPhone)
-	phoneNumber = phoneNumber:gsub("%b<>","")
-	font:write(WIDTH /2, y, phoneNumber, size, 1,1,1,1)
-	y = y + size + size
-	local contactEmail = string.format("%s",element.contactEmail)
-	font:write(WIDTH / 2,y, "Email: " .. contactEmail, size,1,1,1,1)	
-   end
-	counter = counter + 1
-
-    
-end
-
-
