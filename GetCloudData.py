42646d5040a72258b14b2243275c33e724bacb7c
Nick Barron <nickb@kentico.com>
2017-09-27 13:53:15 -0500

    Final Version

--- GetCloudData.py
+++ GetCloudData.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python2.7
-import socket, json, time, requests
+import socket, json, time, requests, locale
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 counter = 1
-project_id = "dfc80764-b4d6-4554-aa5f-805c3b2dbef1"
+error = False
+project_id = "84ebeafd-cad0-47e5-811a-789df7a43ad0"
+locale.setlocale( locale.LC_ALL, "" )
 while True:
-    print "Getting Data From Cloud"
-    response = requests.get("https://deliver.kenticocloud.com/" + project_id + "/items?system.type=coffee")
-    print "got the data"
-    cloudItems = response.json()
-    #print(cloudItems)
+    internetError = False
+    print ("Getting Data From Cloud")
+    try:
+        response = requests.get("https://deliver.kenticocloud.com/" + project_id + "/items?system.type=coffee")
+        print ("got the data")
+        cloudItems = response.json()
+        with open('CloudDataFull.json','w') as outfile:
+            json.dump(cloudItems,outfile)
+        print("CLoud Data Retrieved and stored")
+    except:
+        error = True
+        internetError = True
+        print("Error Getting Items From Cloud")
+        cloudItems = json.loads(open('CloudDataFull.json').read())
+    print(cloudItems)
       
     # elements in the "Coffee" content type
     # Coffee Codename: 'coffee'
     # System: 
     #   name                    i['system']['name']
     # Elements: 
-    #   product_name            i['elements']['product_name']['value']
+    #   coffee_name            i['elements']['coffee_name']['value']
     #   price                   i['elements']['price']['value']
-    #   image[]                 i['elements']['image']['value'][0]['name']
+    #   photo[]                 i['elements']['photo']['value'][0]['name']
+    #   Promotion               i['elements']['promotion']['value'][0]['name']
     #   short_description       
     #   long_description        
     #   product_status (tax)
@@ -27,38 +40,55 @@ while True:
     #   variety
     #   processing
     #   altitude
-    print "Jumping into a loop of data"
+    print ("Jumping into a loop of data")
     for i in cloudItems['items']:
         elements = []
+        convertedPrice = locale.currency(i['elements']['price']['value'], grouping=True)
+        haspromotion = ""
+        hasprocessing = ""
+        try:
+            haspromotion = i['elements']['promotion']['value'][0]['name']
+        except:
+            error = True
+        try:
+            hasprocessing = i['elements']['processing']['value'][0]['name']
+        except:
+            error = True
         elements.append(dict(
-            product_name = i['elements']['product_name']['value'],
-            price = i['elements']['price']['value'],
-            image = i['elements']['image']['value'][0]['name'],
+            coffee_name = i['elements']['coffee_name']['value'],
+            photo = i['elements']['photo']['value'][0]['name'],
             short_description = i['elements']['short_description']['value'],
-            #long_description = i['elements']['long_description']['value'],
-           # product_status (tax)
             farm = i['elements']['farm']['value'],
             country = i['elements']['country']['value'],
-            #variety = i['elements']['variety']['value'],
-            #processing = i['elements']['processing']['value'],
-            #altitude = i['elements']['altitude']['value'],
+            variety = i['elements']['variety']['value'],
+            price = convertedPrice,
+            promotion = haspromotion,
+            processing = hasprocessing,
+            wifiError = internetError,
 	    ))
 
+        #convertedPrice = locale.currency(i['elements']['price']['value'], grouping=True)
+        #elements.append(dict(
+        #     price = convertedPrice,
+        #     ))
         exisitingJson = json.loads(open('CloudData.json').read())
-        print "I have the item as JSON"
+        print ("I have the item as JSON")
         a, b =json.dumps(elements, sort_keys=True),json.dumps(exisitingJson, sort_keys=True)
         #for i in cloudItems['items']:
                 #if a != b:
                  #download image
-        print "I am downloading an image"
-        image = requests.get(i['elements']['image']['value'][0]['url'])
-        with open(i['elements']['image']['value'][0]['name'],"wb") as f:
-            f.write(image.content)
-            f.close()
+        print ("I am downloading an image")
+        try:
+            image = requests.get(i['elements']['photo']['value'][0]['url'])
+            with open(i['elements']['photo']['value'][0]['name'],"wb") as f:
+                f.write(image.content)
+                f.close()
                 #End Download Image
+        except:
+            error = True
         with open('CloudData.json','w') as outfile:
             json.dump(elements,outfile)
-        print "The image is downloaded"
+        print ("The image is downloaded")
         elements = json.dumps(elements, ensure_ascii=False)
         print(elements)
 
@@ -68,7 +98,7 @@ while True:
         SendLoad = coffeeUpdate % elements
     
         sock.sendto(SendLoad.encode("utf8"), ('127.0.0.1', 4444))
-        print "The load is sent!"
+        print ("The load is sent!")
         counter = counter + 1
-        print counter
+        print (counter)
         time.sleep(15)
--- node.lua
+++ node.lua
@@ -2,9 +2,12 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 local json = require "json"
 local font = resource.load_font "font.ttf"
+local fontbold = resource.load_font "fontbold.ttf"
 
 node.alias "KenticoCloud"
 local current_image = resource.load_image("image.jpg")
+local red = resource.load_image("red.png")
+local wifierror = resource.load_image("nowifi.png")
 local cloud_img_name = ""
 local elements = {}
 local counter = 0
@@ -20,27 +23,27 @@ util.data_mapper{
 	update = function(elements)
 		coffeeElements = json.decode(elements)
 		for idx, element in ipairs(coffeeElements) do
-			print(element)
-			print(element.image)
-			local image = string.format("%s", element.image)
+			print("element.photo coming up")
+			print(element.photo)
+			local image = string.format("%s", element.photo)
 			cloud_img_name = image
 			current_image = resource.load_image(cloud_img_name)
 		end
 		
 		if image_align == 0 then
 			image_align = WIDTH / 2
-			text_align = 0
+			text_align = 100
 		else
 			image_align = 0
-			text_align = WIDTH / 2
+			text_align = WIDTH / 2 + 20
 		end
 		image_width = WIDTH / 2 + image_align
-		print(image_align)
+		--print(image_align)
 	end
 }
 
 function node.render()
-	gl.clear(0,0,0,1)
+	gl.clear(1,1,1,1)
 	current_image:draw(image_align,0,image_width,HEIGHT)
 
 	for idx, element in ipairs(coffeeElements) do
@@ -51,17 +54,15 @@ function node.render()
 end
 
 function WriteSlide(element)
-        local y = 300
+        local y = 200
 
- 	font:write(text_align, 10, "New Brews", 100, 1,1,.3,1)
+ 	--font:write(text_align, 10, "New Brews", 100, .1,.1,.1,1)
 	--font:write(30, 110, "Demo Time", 100, .5,.5,.5,1)
-        local productName = string.format("%s", element.product_name)
-        font:write(text_align, y, productName, 50, 1,1,.3,1)
-    	y = y + 50
+        local productName = string.format("%s", element.coffee_name)
+        fontbold:write(text_align, y, productName, 75, .1,.1,.1,1)
+    	y = y + 150
 
-	local price = "$" .. string.format("%s" ,element.price) 
-	font:write(text_align ,y, price, 50,1,1,1,1)
-	y = y +100
+	
 
         local shortDescription = string.format("%s", element.short_description)
 	shortDescription = shortDescription:gsub("%b<>","")
@@ -69,23 +70,39 @@ function WriteSlide(element)
 	
 	local size = 50	
     	for i, line in ipairs(lines) do
-        	font:write(text_align, y, line, size, 1, 1, 1, 1)
+        	font:write(text_align, y, line, size, .1, .1, .1, 1)
         	y = y + size
     	end	
 	y = y + size
 	
-	local farm = string.format("%s",element.farm)
-	farm = farm:gsub("%b<>","")
-	font:write(text_align, y, "Farm:", size, 1,1,1,1)
-	y = y + size
+	--local farm = string.format("%s",element.farm)
+	--farm = farm:gsub("%b<>","")
+	--font:write(text_align, y, "Farm:", size, .1,.1,.1,1)
+	--y = y + size
 
-	font:write(text_align, y, farm, size, 1,1,1,1)
-	y = y + size + size
+	--font:write(text_align, y, farm, size, .1,.1,.1,1)
+	--y = y + size + size
         
-	local country = string.format("%s",element.country)
-	font:write(text_align,y, "Country: " .. country, size,1,1,1,1)
+	--local country = string.format("%s",element.country)
+	--font:write(text_align,y, "Country: " .. country, size,.1,.1,.1,1)
+	--y = y + size 	
 
-        --socket.sleep(10)
+	local variety = string.format("%s", element.variety)
+	font:write(text_align,y, "Variety: " .. variety, size,.1,.1,.1,1)
+	y = y + size + size
+	local price = string.format("%s" ,element.price) .. " / 1lb"
+	fontbold:write(text_align ,y, price, 50,.1,.1,.1,1)
+	y = y +100
+	local processing = string.format("%s", element.processing)
+	fontbold:write(text_align,y, processing, size,.6,.6,.6,1)
+	local promotion = string.format("%s", element.promotion)
+	if promotion == "Featured" then
+		red:draw(20,20,800,100)
+		font:write(130,35,"This Week's Featured Blend",50,1,1,1,1)
+	end
+	if element.wifiError == true then
+		wifierror:draw(0,0,50,50)
+	end
 end
 
 
